eae03c68609ecfc2de464962d71206891960dbff
worgflores <worgflores@gmail.com>
2022-06-22 12:47:49 +0800

    updated bash scripts

--- _res/db-install-script.sh
+++ _res/db-install-script.sh
@@ -4,9 +4,9 @@ cp zzz_databases.tar.gz tempdir
 cd tempdir
 tar -xvf zzz_databases.tar.gz
 
-mysql -u root -p1234 -f < zzz_etracs255.sql && \
-mysql -u root -p1234 -f < zzz_image.sql && \
-mysql -u root -p1234 -f < zzz_notification.sql
+mysql -P 13306 -u root -p1234 -f < zzz_etracs255.sql && \
+mysql -P 13306 -u root -p1234 -f < zzz_image.sql && \
+mysql -P 13306 -u root -p1234 -f < zzz_notification.sql
 
 cd ..
 rm -rf tempdir
