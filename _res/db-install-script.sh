414f5105c72bed170b6bf7566ce425d986b5281b
worgflores <worgflores@gmail.com>
2022-07-03 19:13:37 +0800

    _res: updated

--- _res/db-install-script.sh
+++ _res/db-install-script.sh
@@ -4,11 +4,7 @@ cp zzz_databases.tar.gz tempdir
 cd tempdir
 tar -xvf zzz_databases.tar.gz
 
-docker exec -it  mysql bash && mysql -u root -p1234 -e 'show databases' && exit
-
-#mysql -P 13306 -u root -p1234 -f < zzz_etracs255.sql && \
-#mysql -P 13306 -u root -p1234 -f < zzz_image.sql && \
-#mysql -P 13306 -u root -p1234 -f < zzz_notification.sql
+docker exec -it  mysql bash
 
 cd ..
 rm -rf tempdir
